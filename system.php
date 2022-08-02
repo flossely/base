d0deea606dd23a6c59d57fee6426921ef18a4059
Karl August Nestor <jesuscaesar@outlook.com>
2022-08-02 10:05:24 +1000

    Latest update

--- system.php
+++ system.php
@@ -1,7 +1,9 @@
 <?php
 
+// GETTING START DIRECTORY FOR ALL THE APPS IN THIS PATH
 $startDir = '.';
 
+// GETTING SYSTEM PACKAGE INFORMATION
 $currentSystemInfo = file_get_contents('system.info');
 $currentSystemPkg = file_get_contents($currentSystemInfo.'.pkg');
 $currentSystemPkgD = explode('|[1]|', $currentSystemPkg);
@@ -9,6 +11,7 @@ $currentSystemPkgH = $currentSystemPkgD[0];
 $currentSystemPkgHD = explode('|[2]|', $currentSystemPkgH);
 $currentSystemPkgB = $currentSystemPkgD[1];
 
+// GETTING CURRENT SYSTEM PACKAGE DATA
 $syspkg =
 [
     'author' => $currentSystemPkgHD[0],
@@ -29,38 +32,45 @@ $syspkg =
     'version' => $currentSystemPkgHD[2],
 ];
 
+// GETTING SOUND LIBRARY FROM THE CURRENT DIRECTORY
 $soundlib = str_replace($startDir.'/','',(glob($startDir.'/snd.*.flac')));
 $soundcount = count($soundlib);
 $soundct = $soundcount - 1;
 
+// GETTING CURRENT YEAR
 if (file_exists('year')) {
     $today = file_get_contents('year');
 } else {
     $today = -2000;
 }
 
+// ASSIGNING WEBSITE ID
+$websiteID = basename(__DIR__);
+
+// GETTING WEBSITE TITLE, RETURNING ID IF FOUND NOTHING
 if (file_exists('name')) {
     $websiteTitleFile = file_get_contents('name');
     if ($websiteTitleFile != '') {
         $websiteTitle = $websiteTitleFile;
     } else {
-        $websiteTitle = basename(__DIR__);
+        $websiteTitle = $websiteID;
     }
 } else {
-    $websiteTitle = basename(__DIR__);
+    $websiteTitle = $websiteID;
 }
 
+// CAN WEBSITE PLAY INTERFACE SOUNDS
 if (file_exists('sounds')) {
     $sounds = boolval(file_get_contents('sounds'));
 } else {
     $sounds = false;
 }
 
+// GETTING CURRENT LOCALE FROM FILE
 if (file_exists("locale")) {
     $localeOpen = file_get_contents("locale");
     $locale = ($localeOpen != "") ? $localeOpen : "en";
 } else {
     $locale = "en";
 }
-
 $lingua = $locale;
